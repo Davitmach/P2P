213b0b3976775ce1f961f9cae3e2eeed55a2156a
Davitmach <wvime30@gmail.com>
2026-09-25 20:18:06 +0400

    Add Redis integration in AppService and expose port 3000 in docker-compose for development. Update getHello method to retrieve data from Redis.

--- apps/P2P-bot/src/app.service.ts
+++ apps/P2P-bot/src/app.service.ts
@@ -1,8 +1,15 @@
+import { RedisService } from '@app/redis/redis.service';
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello Wo1111111ld!';
+  constructor(
+    private readonly redis: RedisService,
+  ) {}
+  async getHello() {
+    const ca = await this.redis.get('qaq')
+    console.log(ca)
+     return 1
+    
   }
 }
